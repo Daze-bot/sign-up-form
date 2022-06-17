f0c931008f39bf432414f4e825ef5d5bc5d705c8
Code4Daze <jevon.fillette@gmail.com>
2022-06-17 00:14:26 -0400

    Create conditions for showing password reqs

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let submitBtn = document.querySelector('.submitButton');
 let hiddenBtn = document.querySelector('#hiddenBtn');
+let passwordInput = document.querySelector('#user_password')
+let passwordReq = document.querySelector('#passwordReq');
+let confirmInput = document.querySelector('#confirm_password');
+let confirmReq = document.querySelector('#confirmPass');
 
 submitBtn.addEventListener('click', submitBtnClick);
 
@@ -12,6 +16,35 @@ inputFields.forEach(input => {
   input.addEventListener('input', () => {
     input.classList.remove('ignore');
   })
+})
+
+passwordInput.addEventListener('focus', () => {
+  passwordReq.classList.remove('hidden');
+})
+
+passwordInput.addEventListener('input', () => {
+  if (passwordInput.validity.valid) {
+    passwordReq.classList.add('hidden');
+  }
+  else {
+    passwordReq.classList.remove('hidden');
+  }
+})
+
+passwordInput.addEventListener('input', () => {
+  if (passwordInput.value !== confirmInput.value) {
+    confirmReq.classList.remove('hidden');
+  } else {
+    confirmReq.classList.add('hidden');
+  }
+})
+
+confirmInput.addEventListener('input', () => {
+  if (passwordInput.value !== confirmInput.value) {
+    confirmReq.classList.remove('hidden');
+  } else {
+    confirmReq.classList.add('hidden');
+  }
 })
 
 function submitBtnClick() {
