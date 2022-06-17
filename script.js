9da6cc4df5dbad238982ec0595cbfeeb253fa06a
Code4Daze <jevon.fillette@gmail.com>
2022-06-16 21:38:52 -0400

    Edit input validation for better UX
    Add a red border around invalid inputs, but only when the input is not
    blank.  Add a check mark next to valid inputs when not blank.

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let submitBtn = document.querySelector('.submitButton');
 let hiddenBtn = document.querySelector('#hiddenBtn');
 
-submitBtn.addEventListener('click', () => hiddenBtn.click());
+submitBtn.addEventListener('click', submitBtnClick);
+
+let inputFields = document.querySelectorAll('input');
+let nameInput = document.querySelector('#first_name');
+
+inputFields.forEach(input => {
+  if (input.value === "") {
+    input.classList.add('ignore');
+  }
+  input.addEventListener('input', () => {
+    input.classList.remove('ignore');
+  })
+})
+
+function submitBtnClick() {
+  hiddenBtn.click();
+  inputFields.forEach(input => {
+      input.classList.remove('ignore');
+    })}
